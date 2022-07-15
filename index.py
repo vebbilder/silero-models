85ca00a3fead4dc4ae099d7d8fe4a5895ab98212
vebbilder <66235658+vebbilder@users.noreply.github.com>
2022-07-15 20:18:36 +0300

    Update index.py

--- index.py
+++ index.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 def query_example():
     # if key doesn't exist, returns None
     language = request.args.get('text')
-
+    language2 = request.args.get('id')
     device = torch.device('cpu')
     torch.set_num_threads(4)
     local_file = 'model.pt'
@@ -23,8 +23,6 @@ def query_example():
 
     model = torch.package.PackageImporter(local_file).load_pickle("tts_models", "model")
     model.to(device)
-
-    fileid = request.args.get('id')
     example_text = format(language)
     sample_rate = 48000
     speaker = 'aidar'
@@ -43,7 +41,7 @@ def query_example():
             wf.setframerate(sample_rate)
             wf.writeframes(audio)
 
-    write_wave(path=f'static/wav/'+fileid+'.mp3',
+    write_wave(path=f'static/wav/'+language2+'.mp3',
                audio=(audio * 32767).numpy().astype('int16'),
                sample_rate=sample_rate)
 
@@ -54,7 +52,7 @@ def query_example():
       <source
         src="{}"
         type="audio/mpeg"
-      />'''.format(os.path.join(app.root_path, 'static/wav/', fileid+'.mp3'))
+      />'''.format(os.path.join(app.root_path, 'static/wav/', language2+'.mp3'))
 
 
 @app.route('/form-example')
